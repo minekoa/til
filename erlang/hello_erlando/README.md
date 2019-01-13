8086c8908882316a714ab9aef01d4f8a46538a87
minekoa <mineko.orange@gmail.com>
2019-01-13 17:46:32 +0900

    erlando で do記法。とりあえずビルド通ったよ！

--- erlang/hello_erlando/README.md
+++ erlang/hello_erlando/README.md
@@ -25,4 +25,19 @@ Run
 
 
 
+erlando とは、Erlang で do記法とscheme のcutsマクロを実現するためのライブラリ。
+do記法は maybe なんかで case ネストで大変なことになるのを防いだりとかが
+主な用途。
+
+cutマクロは、Erlang は カリー化サポートしないから クロージャで
+くるむ必要があるのめんどくない？という動機。
+
+
+
+学習課題は以下の記事を参考にした。
+
+[erlando - ErlangでMaybeモナドとdo記法を使う](https://qiita.com/tatsuya6502/items/9f43d1b4486f0416ac68)
+
+
+
 
--- erlang/hello_erlando/src/hello_erlando.erl
+++ erlang/hello_erlando/src/hello_erlando.erl
@@ -1,5 +1,10 @@
 -module(hello_erlando).
 
+-import(maybe_m, ['>>='/2, return/1]).
+
+
+-compile({parse_transform, do}).
+
 %% API exports
 -export([main/1]).
 
@@ -8,10 +13,54 @@
 %%====================================================================
 
 %% escript Entry point
-main(Args) ->
-    io:format("Args: ~p~n", [Args]),
+main(_Arg) ->
+    Ret = routine2(),
+    io:format("Args: ~p~n", [Ret]),
     erlang:halt(0).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+
+-type maybe(A) :: {just, A} | nothing.
+
+
+-type birds() :: integer().
+-type pole() :: {birds(), birds()}.
+
+
+-spec routine2() -> maybe(pole()).
+routine2() ->
+    do([maybe_m ||
+           Start <- return({0, 0}),
+           First <- land_left(2, Start),
+           Second <- land_right(2, First),
+           land_left(1, Second)
+       ]).
+
+
+-spec land_left( 
+        birds(), pole()
+       ) -> maybe( integer() ).
+land_left(N, {Left, Right}) ->
+    case abs((Left + N) - Right) < 4 of
+        true ->
+            {just, {Left + N, Right}};
+        false ->
+            nothing
+    end.
+
+-spec land_right( 
+        birds(), pole()
+       ) -> maybe( pole() ).
+land_right(N, {Left, Right}) ->
+    case abs(Left - (Right + N)) < 4 of
+        true ->
+            {just, {Left, Right + N}};
+        false ->
+            nothing
+    end.
+
+
+
