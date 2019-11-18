2408fccccff861cf22a53e6794d4b7061bb29937
minekoa <mineko.orange@gmail.com>
2019-11-18 23:16:52 +0900

    Rust Book $2 (2nd use crate Rand, match syntax)

---
+++ rust/rustbook/guessing_game/README.md
@@ -0,0 +1,21 @@
+
+Cargo.toml
+
+```
+[dependencies]
+
+rand = "0.3.14"
+```
+
+```
+$ cargo build
+    Updating crates.io index
+  Downloaded rand v0.3.23
+  Downloaded libc v0.2.65
+  Downloaded rand v0.4.6
+   Compiling libc v0.2.65
+   Compiling rand v0.4.6
+   Compiling rand v0.3.23
+   Compiling guessing_game v0.1.0 (/home/zoniathena/reps/til/rust/rustbook/guessing_game)
+    Finished dev [unoptimized + debuginfo] target(s) in 35.27s
+```
--- rust/rustbook/guessing_game/src/main.rs
+++ rust/rustbook/guessing_game/src/main.rs
@@ -1,15 +1,30 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number! (数を当ててご覧)");
 
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is : {}", secret_number);
+
     println!("Please input your guess. (予想を入力してね)");
-
     let mut guess = String::new();
-
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line (行の読み込みに失敗しました)");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number! (数値を入力してね)");
+
     println!("You gessed (あなたの予想): {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small! (ちいさすぎ)"),
+        Ordering::Greater => println!("Too big! (おおきすぎ)"),
+        Ordering::Equal   => println!("You win! (やったねたえちゃん！）"),
+    }
+
 }
